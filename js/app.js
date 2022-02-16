86f0ef907709aafdde406d2d870825c550fbba4b
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-02-17 02:29:02 +0600

    some javascript code added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log('hello world!!');
+
+document.getElementById("calculate-btn").addEventListener("click", function () {
+  const income = document.getElementById("my-income").value;
+  const food = document.getElementById("food").value;
+  const rent = document.getElementById("rent").value;
+  const clothes = document.getElementById("clothes").value;
+  const totalExpenses =
+    parseFloat(food) + parseFloat(rent) + parseFloat(clothes);
+  const displayExpenses = document.getElementById("total-expenses");
+  displayExpenses.innerText = totalExpenses;
+  const displayBalance = document.getElementById("balance");
+  displayBalance.innerText = income - totalExpenses;
+});
+document.getElementById()
